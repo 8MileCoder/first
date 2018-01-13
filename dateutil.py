bf500761a1cf456793e0a567e5164571d6d9c867
Amrinder Singh Bedi <13amrindersinghbedi@gmail.com>
2018-01-13 16:37:10 +0530

    added two more funtions for timestamp and month

--- dateutil.py
+++ dateutil.py
@@ -1,3 +1,6 @@
 # a function that will return the current date
 
 # a function that will return the current year
+
+# a fucntion that will return the timestamp
+# a function that will return the current month
